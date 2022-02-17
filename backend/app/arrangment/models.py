dc08a0d72bf1b81f90733049394380adbcfe2511
zmarusic <zeljkomarusic@hotmail.com>
2022-02-17 13:52:27 +0100

    fst-01 add room model

--- backend/app/arrangment/models.py
+++ backend/app/arrangment/models.py
@@ -1,5 +1,5 @@
-from typing import Optional
-from sqlmodel import Field, SQLModel
+from typing import List, Optional
+from sqlmodel import Field, Relationship, SQLModel
 
 
 class LocationBase(SQLModel):
@@ -8,3 +8,16 @@ class LocationBase(SQLModel):
 
 class Location(LocationBase, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
+
+    rooms: List["Room"] = Relationship(back_populates="location")
+
+
+class RoomBase(SQLModel):
+    name: str = Field(index=True)
+    location_id: Optional[int] = Field(default=None, foreign_key="location.id")
+
+
+class Room(RoomBase, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+
+    location: Optional[Location] = Relationship(back_populates="rooms")
