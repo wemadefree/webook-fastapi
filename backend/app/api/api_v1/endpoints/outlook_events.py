50aa97ebf9def1fed6aab99bbf242725aa69ee89
DESKTOP-T8VR2O2\Magnus Harlem <magnus@wemade.no>
2023-03-15 11:04:29 +0100

    fix wrong keyword

--- backend/app/api/api_v1/endpoints/outlook_events.py
+++ backend/app/api/api_v1/endpoints/outlook_events.py
@@ -25,7 +25,7 @@ def get_outlook_events_for_user(
         return None
 
     response: httpx.Response = graph_client.exhaust(
-        url=f"https://graph.microsoft.com/v1.0/users/{user_object_id}/calendar/calendarView?StartDateTime={start_datetime_iso_8601}&EndDateTime={end_datetime_iso_8601}"
+        f"https://graph.microsoft.com/v1.0/users/{user_object_id}/calendar/calendarView?StartDateTime={start_datetime_iso_8601}&EndDateTime={end_datetime_iso_8601}"
     )
 
     return response.json()
